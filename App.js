d2fff10354c1c098fbdcd96e67ac4a539867d610
vumbumy <vumbumy@outlook.kr>
2022-01-17 01:11:15 +0900

    Add content and billing

--- App.js
+++ App.js
@@ -8,10 +8,12 @@ import {Row, Col, Tabs} from 'antd';
 const { TabPane } = Tabs;
 
 import Account from './assets/api/account.yaml'
+import Group from './assets/api/group.yaml'
+import Content from './assets/api/content.yaml'
+import Billing from './assets/api/billing.yaml'
 import Category from './assets/api/category.yaml'
 import Constant from './assets/api/constant.yaml'
 import Event from './assets/api/event.yaml'
-import Group from './assets/api/group.yaml'
 import Process from './assets/api/process.yaml'
 
 export default function App() {
@@ -22,19 +24,25 @@ export default function App() {
             <TabPane tab="Account" key="1">
               <SwaggerUI url={Account} />
             </TabPane>
-            <TabPane tab="Category" key="2">
-              <SwaggerUI url={Category} />
-            </TabPane>
-            <TabPane tab="Constant" key="3">
-              <SwaggerUI url={Constant} />
-            </TabPane>
-            <TabPane tab="Event" key="4">
-              <SwaggerUI url={Event} />
-            </TabPane>
-            <TabPane tab="Group" key="5">
+            <TabPane tab="Group" key="2">
               <SwaggerUI url={Group} />
             </TabPane>
-            <TabPane tab="Process" key="6">
+            <TabPane tab="Category" key="3">
+              <SwaggerUI url={Category} />
+            </TabPane>
+            <TabPane tab="Content" key="4">
+              <SwaggerUI url={Content} />
+            </TabPane>
+            <TabPane tab="Billing" key="5">
+              <SwaggerUI url={Billing} />
+            </TabPane>
+            <TabPane tab="Constant" key="6">
+              <SwaggerUI url={Constant} />
+            </TabPane>
+            <TabPane tab="Event" key="7">
+              <SwaggerUI url={Event} />
+            </TabPane>
+            <TabPane tab="Process" key="8">
               <SwaggerUI url={Process} />
             </TabPane>
           </Tabs>
